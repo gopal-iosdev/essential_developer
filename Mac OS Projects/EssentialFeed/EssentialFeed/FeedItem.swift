7f3a0d628d85e0c52f7ae9856fe448759d1975e3
gopal-iosdev <gopal.iosdev@gmail.com>
2022-10-20 11:53:18 -0400

    Create FeedItem modesl

---
+++ Mac OS Projects/EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Gopal Gurram on 10/20/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
