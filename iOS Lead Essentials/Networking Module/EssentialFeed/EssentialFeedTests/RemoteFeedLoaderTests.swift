1e558aadb2446dd186a800f71f2c5fae087d7291
gopal-iosdev <gopal.iosdev@gmail.com>
2022-12-17 01:11:05 -0500

    Capturing Requested URL's

--- iOS Lead Essentials/Networking Module/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ iOS Lead Essentials/Networking Module/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -14,7 +14,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let url = URL(string: "https://a-url.com")!
         let (_, client) = makeSUT(url: url)
         
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     func test_load_requestsDataFromURL() {
@@ -23,7 +23,17 @@ final class RemoteFeedLoaderTests: XCTestCase {
         
         sut.load()
         
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    func test_loadTwice_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
     // MARK: Helpers
@@ -36,10 +46,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HttpClientSpy: HttpClient {
-        var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
-            requestedURL = url
+            requestedURLs.append(url)
         }
     }
     
