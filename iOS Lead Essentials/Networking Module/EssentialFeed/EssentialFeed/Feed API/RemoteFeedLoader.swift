f167e5bd61e09d6b8802c138a001e6e5b6eee4ec
gopal-iosdev <gopal.iosdev@gmail.com>
2022-12-19 22:15:34 -0500

    Loading error on client error

--- iOS Lead Essentials/Networking Module/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ iOS Lead Essentials/Networking Module/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -11,16 +11,28 @@ public final class RemoteFeedLoader {
     private let url: URL
     private let client: HttpClient
     
+    public enum Error: Swift.Error {
+        case connectivity
+    }
+    
     public init(url: URL, client: HttpClient) {
         self.url = url
         self.client = client
     }
     
-    public func load() {
-        client.get(from: url)
+    public func load(completion: @escaping (Error) -> Void = { _ in }) {
+        client.get(from: url) { error in
+            let result: Error
+            
+            defer {
+                completion(result)
+            }
+            
+            result = .connectivity
+        }
     }
 }
 
 public protocol HttpClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Error) -> Void)
 }
