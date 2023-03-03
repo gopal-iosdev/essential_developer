0686fda6d00f560d9a32a44619ada654724206f4
gopal-iosdev <gopal.iosdev@gmail.com>
2023-03-03 09:05:38 -0500

    Created ApiClient as Singleton

---
+++ iOS Lead Essentials/PlanningAndSystemDesign/Singleton.playground/Pages/Video Lecture.xcplaygroundpage/Contents.swift
@@ -0,0 +1,60 @@
+//: [Previous](@previous)
+
+/*
+ Source: https://academy.essentialdeveloper.com/courses/447455/lectures/8528509
+ 
+ Section: Video Lecture
+ 
+ */
+
+import UIKit
+
+// MARK: -  Singleton
+
+struct LoggedInUser {}
+
+struct FeedItem {}
+
+class ApiClient {
+    static let instance = ApiClient()
+    
+    private init() {}
+    
+    func login(completion: (LoggedInUser) -> Void) {}
+    func loadFeed(completion: (FeedItem) -> Void) {}
+}
+
+let client = ApiClient.instance
+
+// MARK: - singleton
+
+let urlSession = URLSession.shared
+
+let urlSessionTwo = URLSession()
+
+// MARK: - Testing Singleton
+
+class LoginViewController: UIViewController {
+    var api = ApiClient.instance
+    
+    func didTapLogin() {
+        api.login { user in
+            // show feed screen
+        }
+    }
+}
+
+class FeedViewController: UIViewController {
+    var api = ApiClient.instance
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        api.loadFeed { feedItems in
+            // Show Feed
+        }
+    }
+}
+
+
+//: [Next](@next)
