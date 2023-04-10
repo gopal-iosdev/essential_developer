ad0c272510996a5d8a5b10cbd27beb8afdabd325
gopal-iosdev <gopal.iosdev@gmail.com>
2023-04-09 22:16:23 -0400

    Created 05_Developer_Before_BE playground

---
+++ iOS Lead Essentials/PlanningAndSystemDesign/Playgrounds/05_Developer_Before_BE.playground/Pages/Comments.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+/*
+ Source: https://academy.essentialdeveloper.com/courses/447455/lectures/8469954
+
+ Section: Comments
+
+ */
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
---
+++ iOS Lead Essentials/PlanningAndSystemDesign/Playgrounds/05_Developer_Before_BE.playground/Pages/Video Lecture.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+/*
+ Source: https://academy.essentialdeveloper.com/courses/447455/lectures/8469954
+
+ Section: Video Lecture
+
+ */
+
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
