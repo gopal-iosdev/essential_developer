a1869ce18d85c1d53d7ffff03b3eb0e2408ea912
gopal-iosdev <gopal.iosdev@gmail.com>
2023-04-14 11:05:51 -0400

    Updated 05_Develop_Before_BE with comments

--- iOS Lead Essentials/PlanningAndSystemDesign/Playgrounds/05_Develop_Before_BE.playground/Pages/Comments.xcplaygroundpage/Contents.swift
+++ iOS Lead Essentials/PlanningAndSystemDesign/Playgrounds/05_Develop_Before_BE.playground/Pages/Comments.xcplaygroundpage/Contents.swift
@@ -9,6 +9,109 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+// MARK: - Loading Images
+
+enum FeedImageSource {
+    case url(URL)
+    case data(Data)
+}
+
+struct FeedImage {
+    let id: UUID
+    let location: String?
+    let description: String?
+    let source: FeedImageSource
+}
+
+// MARK: - Protocols for data models will complicate the design
+
+// Problem
+
+/*
+
+protocol message {
+    var id :Int {get set}
+    var date:String {get set}
+    var senderName:String {get set}
+    var senderImageUrl:String {get set }
+}
+
+struct textMessage: message {
+    var id: Int
+
+    var date: String
+
+    var senderName: String
+
+    var senderImageUrl: String
+
+    var body:String
+}
+
+struct fileMessage: message {
+    var id: Int
+
+    var date: String
+
+    var senderName: String
+
+    var senderImageUrl: String
+
+    var fileUrl:String
+}
+
+ */
+
+// Solution - Using Structs & Enum
+
+/*
+
+struct TextMessage {
+    let id: Int
+    let date: String
+    let senderName: String
+    let senderImageUrl: String
+    let body:String
+}
+
+struct FileMessage {
+    let id: Int
+    let date: String
+    let senderName: String
+    let senderImageUrl: String
+    let fileUrl:String
+}
+
+enum Message {
+  case text(TextMessage)
+  case file(FileMessage)
+}
+
+ */
+
+// MARK: - Modelling an example response(i.e. refer news.json in Resources folder)
+
+struct NewsResponse {
+    let status: String
+    let totalResults: Int
+    let news: [NewsItem]
+}
+
+struct NewsItem {
+    let source: Source
+    let tags: [Tag]?
+    let author: String
+    let title: String
+}
+
+struct Source {
+    let id: UUID?
+    let name: String
+}
+
+struct Tag {
+    let id: UUID
+    let name: String
+}
 
 //: [Next](@next)
