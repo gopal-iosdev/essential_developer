ff13ee4003d957f39f27efe1ca494f52924a5232
gopal-iosdev <gopal.iosdev@gmail.com>
2023-04-15 14:37:51 -0400

    Created FeedItem model

---
+++ iOS Lead Essentials/essential-feed-case-study/EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Gopal Gurram on 4/15/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
