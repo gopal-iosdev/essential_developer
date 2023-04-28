2b67b18a7f1bdcdb15088e5cce5c8db949204219
gopal-iosdev <gopal.iosdev@gmail.com>
2023-04-28 10:41:40 -0400

    Code refactoring

--- iOS Lead Essentials/essential-feed-case-study/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ iOS Lead Essentials/essential-feed-case-study/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -16,7 +16,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
 
-    func test_init_requestsDataFromURL() {
+    func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
 
