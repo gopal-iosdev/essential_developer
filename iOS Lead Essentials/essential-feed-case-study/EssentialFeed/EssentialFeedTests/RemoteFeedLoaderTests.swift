3e658d1092c44ab966aaebffa1e4f1068c828f9b
gopal-iosdev <gopal.iosdev@gmail.com>
2023-06-05 09:33:44 -0400

    Delivers invalidData error on 200 http response with invalid json

--- iOS Lead Essentials/essential-feed-case-study/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ iOS Lead Essentials/essential-feed-case-study/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -61,6 +61,17 @@ final class RemoteFeedLoaderTests: XCTestCase {
         })
     }
 
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+
+        let invalidJSON = Data("invalid json".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
     // MARK: - Helpers
 
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -83,7 +94,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(clientError))
         }
 
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let urlResponse = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
@@ -91,7 +102,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
                 headerFields: nil
             )!
 
-            messages[index].completion(.success(urlResponse))
+            messages[index].completion(.success(data, urlResponse))
         }
     }
 }
