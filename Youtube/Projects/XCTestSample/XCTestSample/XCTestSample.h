0247c7389e192f60ba46bdc3f7415babccb319d2
gopal-iosdev <gopal.iosdev@gmail.com>
2023-08-01 09:05:44 -0400

    Created XCTestSample project

---
+++ Youtube/Projects/XCTestSample/XCTestSample/XCTestSample.h
@@ -0,0 +1,18 @@
+//
+//  XCTestSample.h
+//  XCTestSample
+//
+//  Created by Gopal Gurram on 8/1/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XCTestSample.
+FOUNDATION_EXPORT double XCTestSampleVersionNumber;
+
+//! Project version string for XCTestSample.
+FOUNDATION_EXPORT const unsigned char XCTestSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCTestSample/PublicHeader.h>
+
+
